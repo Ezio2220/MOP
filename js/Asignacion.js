cdbda12a25fd41f2c5e3950c84a345deda9dbef5
Alejandro0112 <43940986+Alejandro0112@users.noreply.github.com>
2019-06-09 17:55:48 -0600

    Update Asignacion.js

--- js/Asignacion.js
+++ js/Asignacion.js
@@ -41,7 +41,7 @@ function GeneraR(){
     contenido+="</tbody> </table>";
 //-------------------------------------------------DIBUJA LAS OPCIONES DE MINIMZAR MAXIMIZAR y el boton resolver!----------------    
     //alert(contenido); esto muestra lo que manda
-    contenido+="<center><button id='gen2' onclick='Generar2("+NV+","+NR+");' type='button' rel='tooltip' class='btn btn-info'> RESOLVER! </button> </center> <br>"
+    contenido+="<center><button id='gen2' onclick='GeneraR2("+NV+","+NR+");' type='button' rel='tooltip' class='btn btn-info'> RESOLVER! </button> </center> <br>"
     plano.innerHTML=contenido;
 }
 
@@ -78,4 +78,212 @@ function comp2(nv,nr,t,obj){
     }while(n==0);
     
 }
+function GeneraR2(nf,nc){
 
+    NVF = nv;
+    
+        var Bi = [];
+        var type = [];  
+        var rest = [];
+        var Z = new Array(nv); 
+        for(var i=0;i<nr;i++){
+            rest.push(new Array(nv)); 
+            type.push(document.getElementById("t"+i).value);
+            Bi.push(Number(document.getElementById("n"+i+"*"+nv).value));
+        }
+        Bi.push(document.getElementById("Z"+nv).value);
+        for(var i=0;i<nr;i++){
+    
+            for(var j=0;j<nv;j++){
+                rest[i][j]=document.getElementById("n"+i+"*"+j).value; 
+                Z[j]= document.getElementById("Z"+j).value;
+            }
+        }
+        
+     
+    var pos=[];
+    
+        for(var x=0;x<nr;x++){
+            if(type[x]== 1){
+                NVF+=1;
+                for(var i=0;i<nr;i++){
+                    if(i==x){
+                        rest[i].push(1);
+                    }else{
+                        rest[i].push(0);
+                    }
+                }
+                Z.push(0);
+                pos.push(NVF);
+            }
+            
+            if(type[x]==2){
+    
+                NVF+=2;
+                for(var i=0;i<nr;i++){
+                    if(i==x){
+                        rest[i].push(-1);
+                        rest[i].push(1);
+                    }else{
+                        rest[i].push(0);
+                        rest[i].push(0);
+                    }
+                }
+                Z.push(0);
+    
+                pos.push(NVF);
+            }
+            if(type[x]==3){
+    
+                NVF+=1;
+                for(var i=0;i<nr;i++){
+                    if(i==x){
+                        
+                        rest[i].push(1);
+                    }else{
+                        rest[i].push(0);
+                    }
+                }
+                
+    
+                pos.push(NVF);
+            }
+        }
+        
+        console.log(rest);
+        console.log(Bi);
+        console.log(Z);
+        console.log(pos);
+        var M = [];
+        for(var i=0;i<=NVF;i++){        
+            M.push(Math.round(0));
+        }
+        console.log(M);
+        var contenido2 = " ";
+        contenido2+=" <h3><u>SOL</u></h3> <br> ";
+        for(var i=0;i<nr;i++){
+            contenido2+=(i+1)+")";
+            for(var j=0;j<NVF;j++){
+                if(rest[i][j]!=0){
+                    
+                    if(rest[i][j] == 1){
+                        if(j!=0){
+                            contenido2+="+";
+                        }
+                        contenido2+= "X"+(j+1);
+                    }else if(rest[i][j] == -1){
+                        contenido2+= "-X"+(j+1);
+                    }else{
+                        if(j!=0){
+                            contenido2+="+";
+                        }
+                        contenido2+= rest[i][j]+"X"+(j+1);
+                    }
+                    
+                }
+                
+            }
+            contenido2+="<br>";
+        }
+        
+        var plano2 = document.getElementById("Stabla4");
+    
+        contenido2 += "<table class='table'> <thead> <tr> <th class='text-center mx-auto'>V.b</th> ";
+        
+        for(var i=0;i<NVF;i++){
+            contenido2+="<th class='text-center'>X"+(i+1)+"</th>";
+        }
+        contenido2+="<th class='text-center'>Bi</th> </tr> </thead> <tbody>";
+    
+        for(var i=0;i<nr;i++){
+            contenido2+="<tr> <td class='text-center'>X"+pos[i]+"</td>";
+            for(var j=0;j<NVF;j++){
+                contenido2+="<td class='text-center'> "+rest[i][j]+" </td>";
+            }
+            contenido2+="<td class='text-center'> "+Bi[i]+" </td></tr>";
+        }
+        contenido2+="<tr><td class='text-center'><b>Z</b></td>";
+        for(var i=0;i<NVF;i++){
+            contenido2+="<td class='text-center'> "+Z[i]+" </td>";
+        }
+        contenido2+="<td class='text-center'> "+Bi[nr]+"</td> </tr> </tbody> </table> </br></br>";
+    
+        plano2.innerHTML = contenido2;
+    //----------------------------------------------------------------------------------------------------
+        
+        
+    
+        contenido2 += "<table class='table'> <thead> <tr> <th class='text-center mx-auto'>V.b</th> ";
+        
+        for(var i=0;i<NVF;i++){
+            contenido2+="<th class='text-center'>X"+(i+1)+"</th>";
+        }
+        contenido2+="<th class='text-center'>Bi</th> </tr> </thead> <tbody>";
+    
+        for(var i=0;i<nr;i++){
+            contenido2+="<tr> <td class='text-center'>X"+pos[i]+"</td>";
+            for(var j=0;j<NVF;j++){
+                contenido2+="<td class='text-center'> "+rest[i][j]+" </td>";
+            }
+            contenido2+="<td class='text-center'> "+Bi[i]+" </td></tr>";
+        }
+        contenido2+="<tr><td class='text-center'><b>Z</b></td>";
+        var s = "+";
+        for(var i=0;i<NVF;i++){
+            if(M[i]>0 && Z[i]!= 0){
+                s="+";
+            }else{
+                s="";
+            }
+            contenido2+="<td class='text-center'> ";
+            if(Z[i]!=0 || M[i]==0){
+                contenido2+=Z[i];
+            }
+            if(M[i]!=0){
+    
+                
+                if(M[i]!= 1 && M[i] != -1){
+                    contenido2+=s+M[i];
+                }else{
+                    if(M[i]==-1){
+                        contenido2+="-";
+                    }else{
+                        contenido2+="+";
+                    }
+                }
+                contenido2+="M </td>";
+            }else{
+                contenido2+=" </td>";
+            }
+            
+        }
+        contenido2+="<td class='text-center'> ";
+        if(M[NVF]>0 && Bi[nr] != 0 ){
+            s="+";
+        }else{
+            s="";
+        }
+        if(Bi[nr]!=0 || M[NVF]==0 ){
+            contenido2+=Bi[nr];
+        }
+        if(M[NVF]!=0){
+            
+            if(M[NVF]!=1 && M[NVF]!= -1){
+                contenido2+=s+M[NVF];
+            }else{
+                if(M[NVF]==-1){
+                    contenido+="-";
+                }else{
+                    contenido2+="+";
+                }
+            }
+            contenido2+="M </td> </tr> </tbody> </table> </br></br>";
+        }else{
+            contenido2+="</td> </tr> </tbody> </table> </br></br>";
+        }
+    
+        plano2.innerHTML = contenido2;
+
+
+
+}
