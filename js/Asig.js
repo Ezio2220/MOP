84b8768fa05117b8858b0c16ff710ff882aa2214
Ezio2220 <fjosue447@gmail.com>
2019-06-27 12:19:21 -0600

    Update Asig.js

--- js/Asig.js
+++ js/Asig.js
@@ -29,16 +29,23 @@ function Agen(ndat,pro1,pro2,dest,xh,yh){
     Y=yh;
     var X = [];
     X=xh;
-    
+    //AQUIE se crea la variable que sera un arreglo de todas las letras del abecedario
     var abc = ['A','B','C','D','E','F','G','H','I','J','K','L','M','N','O','P','Q','R','S','T','U','V','W','X','Y','Z',]
     if(ndat ==2){
         d1 = document.getElementById(pro1).value;
         d2 = document.getElementById(pro2).value;
         for(var i=1;i<d1;i++){
-            X.push(abc[i]);
+            X.push(Number(i+1));
         }
-        for(var i=1;i<d2;i++){
-            Y.push(Number(i+1));
+        if(X[0]=="Destino"){
+            X.push("OFERTA");
+        }
+        for(var i=1;i<d2;i++){ //aqui va a iterar donde i es el numero que va pasando
+            Y.push(abc[i]); //aqui yo hago que en el arreglo Y se agrege una letra del abecedario que coincida con la posicion i
+            //si i=0 entonces se agregara abc[0] lo cual representa la letra A
+        }
+        if(Y[0]=="Origen"){
+            Y.push("DEMANDA");
         }
     }else{
         d1 = document.getElementById(pro1).value;
@@ -46,7 +53,7 @@ function Agen(ndat,pro1,pro2,dest,xh,yh){
     
         for(var i=1;i<d1;i++){
             
-            X.push(abc[i]);
+            X.push(abc[i]);  //aca lo mismo se agrega dentro del nuevo arreglo la letra del abecedari que corresponda a la posicion i
             Y.push(Number(i+1));
         }
     }
@@ -54,7 +61,7 @@ function Agen(ndat,pro1,pro2,dest,xh,yh){
     if(d1.length==0 || d1<=1){
         obj.innerHTML=" ";
     }else{
-        
+        //ya aca abajo dibujo la tabla segun los datos del arreglo
         var table="";
 
         table += "<table style='background: none;' class='table'><thead> <tr> ";
@@ -65,9 +72,19 @@ function Agen(ndat,pro1,pro2,dest,xh,yh){
         }
         table+="</tr> </thead> <tbody>";
         for(var i=1;i<Y.length;i++){
-            table+="<tr><td class='text-center mx-auto'>"+i+"</td>";
+            if(Y[i]=="DEMANDA"){
+                table+="<tr><td class='text-center mx-auto'>"+Y[i]+"</td>";
+            }else{
+                table+="<tr><td class='text-center mx-auto'>"+i+"</td>";
+            }
+            
             for(var j=1;j<X.length;j++){
-                table+="<td> <input onchange='' type='number' id='n"+j+"*"+i+"'> </td>"
+                if(X[j]=="OFERTA" && Y[i]=="DEMANDA"){
+                    table+= "<td> </td>";
+                }else{
+                    table+="<td> <input onchange='' type='number' id='n"+j+"*"+i+"'> </td>";
+                }
+                
             }
             table+="</tr>";
         }
